78472abc0a783b94e5b3bfd64a67914f4697e4db
gavlig <gavlig@gmail.com>
2022-11-27 14:56:06 +0100

    use native/own helix runtime in run.sh instead of relying on system

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-RUST_BACKTRACE=1 HELIX_RUNTIME=/var/lib/helix/runtime/ cargo run -r -- /home/gavlig/Workspace/project_kodiki/kodiki/src/bevy_helix/render.rs
+RUST_BACKTRACE=1 HELIX_RUNTIME=/home/gavlig/Workspace/project_kodiki/kodiki/third_party/helix-editor/runtime cargo run -r -- /home/gavlig/Workspace/project_kodiki/kodiki/src/bevy_helix/render.rs
