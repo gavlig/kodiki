b844cdb8e23fb372538d96f667f6a0e08453ee19
Nataliia Vyshtak <nat.vyshtak@gmail.com>
2023-11-18 21:01:17 +0100

    mac fixes

--- prepare_runtime.sh
+++ prepare_runtime.sh
@@ -1,10 +1,12 @@
 mkdir build
 mkdir runtime_temp
 cd ./runtime_temp
-wget https://github.com/helix-editor/helix/releases/download/23.05/helix-23.05-x86_64-linux.tar.xz
+# wget https://github.com/helix-editor/helix/releases/download/23.05/helix-23.05-x86_64-linux.tar.xz
+curl -LJO https://github.com/helix-editor/helix/releases/download/23.05/helix-23.05-x86_64-linux.tar.xz 
 tar -xvf helix-23.05-x86_64-linux.tar.xz
 rm helix-23.05-x86_64-linux.tar.xz
-cp ./helix-23.05-x86_64-linux/runtime ../build/bin/ -r
-cp ../assets/ ../build/bin/ -r
+mkdir ../build/bin/
+cp -R ./helix-23.05-x86_64-linux/runtime ../build/bin/
+cp -R ../assets ../build/bin/
 cd ../
-rm runtime_temp -r
+rm -r runtime_temp
