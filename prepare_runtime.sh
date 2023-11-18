fcf5db317d9a1ee2421b6cebb6aca8bc6c921c58
Nataliia Vyshtak <nat.vyshtak@gmail.com>
2023-11-18 21:36:45 +0100

    more adjustable prepare-runtime.sh

--- prepare_runtime.sh
+++ prepare_runtime.sh
@@ -1,12 +1,17 @@
+PLATFORM="aarch64"
+OS="macos"
+
+# PLATFORM = "x86_64"
+# OS = "linux"
+
 mkdir build
 mkdir runtime_temp
 cd ./runtime_temp
-# wget https://github.com/helix-editor/helix/releases/download/23.05/helix-23.05-x86_64-linux.tar.xz
-curl -LJO https://github.com/helix-editor/helix/releases/download/23.05/helix-23.05-x86_64-linux.tar.xz 
-tar -xvf helix-23.05-x86_64-linux.tar.xz
-rm helix-23.05-x86_64-linux.tar.xz
+curl -LJO https://github.com/helix-editor/helix/releases/download/23.05/helix-23.05-$PLATFORM-$OS.tar.xz
+tar -xvf helix-23.05-$PLATFORM-$OS.tar.xz
+rm helix-23.05-$PLATFORM-$OS.tar.xz
 mkdir ../build/bin/
-cp -R ./helix-23.05-x86_64-linux/runtime ../build/bin/
+cp -R ./helix-23.05-$PLATFORM-$OS/runtime ../build/bin/
 cp -R ../assets ../build/bin/
 cd ../
 rm -r runtime_temp
