4ba35eaeea55e7e32ee1e8d015e7361a38550e8d
gavlig <gavlig@gmail.com>
2023-12-22 22:02:05 +0100

    fixed vertically inverted mouse cursor picking

--- src/kodiki_ui/raypick/systems_utils.rs
+++ src/kodiki_ui/raypick/systems_utils.rs
@@ -9,7 +9,7 @@ pub fn ray_from_mouse_position(
     let mouse_position = window.cursor_position().unwrap_or(Vec2::new(0.0, 0.0));
 
     let x = 2.0 * (mouse_position.x / window.width() as f32) - 1.0;
-    let y = 2.0 * (mouse_position.y / window.height() as f32) - 1.0;
+    let y = (2.0 * (mouse_position.y / window.height() as f32) - 1.0) * -1.0; // multiplied by -1 because this is all pre bevy 0.11 logic and Window::cursor_position has started being relative to the top left instead of bottom left
 	let mouse_position_norm = Vec2::new(x, y);
 
     let camera_inverse_matrix = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
